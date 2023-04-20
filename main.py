15869030b63028e3ed03b5e3a56b125f012eb628
Rodrigo Moreirao <rmoreirao@microsoft.com>
2023-04-20 12:50:46 +0200

    adding colors..

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import sys
 
 def main():
     print("Current version: " + str(sys.version_info))
-    if sys.version_info[:2] != (3,7):
-        print("Python 3.7 is required. Exiting...")
+    if sys.version_info[:2] != (3,7): 
+        # print red text to console
+        print("\033[91m" + "Python 3.7 is required. Exiting..." + "\033[0m")
         sys.exit(1)
     else:
-        print("Python 3.7 is installed. Continuing...")
+        # print green text to console
+        print("\033[92m" + "Python 3.7 is installed. Continuing..." + "\033[0m")
 
 if __name__ == "__main__":
     main()
